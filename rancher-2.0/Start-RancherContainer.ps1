f52382bfcf05a6765ec43e2100d1f88e9201bf9f
Jason L. van Brackel <jason@vanbrackel.net>
2018-05-25 09:58:13 -0400

    Update images to v2.0.2.

--- rancher-2.0/Start-RancherContainer.ps1
+++ rancher-2.0/Start-RancherContainer.ps1
@@ -1,2 +1,2 @@
-docker run --restart=unless-stopped -p 8080:80 -p 8443:443 -v ./rancher:/var/lib/rancher  rancher/rancher
+docker run --restart=unless-stopped -p 8080:80 -p 8443:443 -v ./rancher:/var/lib/rancher  rancher/rancher:v2.0.2
 Write-Host "Rancher will be listening on http://locahost:8080 and https://localhost:8443"
--- rancher-2.0/start-rancher-container.sh
+++ rancher-2.0/start-rancher-container.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Start Rancher 2.0 Server in a single docker container
-sudo docker run --restart=unless-stopped -p 8080:80 -p 8443:443 rancher/rancher -v ./rancher:/var/lib/rancher
+sudo docker run --restart=unless-stopped -p 8080:80 -p 8443:443 rancher/rancher:v2.0.2 -v ./rancher:/var/lib/rancher
 echo "Rancher will be listening on http://locahost:8080 and http://localhost:8443."
